abddc02fedc5d773b28f9e560365812e06d3d0d4
akshaypingle88 <akshaypingle88@gmail.com>
2014-06-26 12:30:47 +0530

    Pagination and filter

--- public/app/directives.js
+++ public/app/directives.js
@@ -39,6 +39,58 @@ angular.module('myapp.directives', [])
 
                 configureGrid();
 
+                // filter and Pagination
+//                $scope.filterOptions = {
+//                    filterText: "",
+//                    useExternalFilter: true
+//                };
+//                $scope.totalServerItems = 0;
+//                $scope.pagingOptions = {
+//                    pageSizes: [5, 20, 30],
+//                    pageSize: 5,
+//                    currentPage: 1
+//                };
+//                $scope.setPagingData = function(data, page, pageSize){
+//                    var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+//                    $scope.myData = pagedData;
+//                    $scope.totalServerItems = data.length;
+//                    if (!$scope.$$phase) {
+//                        $scope.$apply();
+//                    }
+//                };
+//                $scope.getPagedDataAsync = function (pageSize, page, searchText) {
+//                    setTimeout(function () {
+//                        var data;
+//                        if (searchText) {
+//                            var ft = searchText.toLowerCase();
+//                            $http.get(attrs.ajaxDataUrl).success(function (largeLoad) {
+//                                data = largeLoad.filter(function(item) {
+//                                    return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+//                                });
+//                                $scope.setPagingData(data,page,pageSize);
+//                            });
+//                        } else {
+//                            $http.get(attrs.ajaxDataUrl).success(function (largeLoad) {
+//                                $scope.setPagingData(largeLoad,page,pageSize);
+//                            });
+//                        }
+//                    }, 100);
+//                };
+//
+//                $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+//
+//                $scope.$watch('pagingOptions', function (newVal, oldVal) {
+//                    if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
+//                        $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+//                    }
+//                }, true);
+//                $scope.$watch('filterOptions', function (newVal, oldVal) {
+//                    if (newVal !== oldVal) {
+//                        $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+//                    }
+//                }, true);
+
+
                 function configureGrid() {
                     debugger;
                     var xmlhttp = new XMLHttpRequest();
--- routes/customer.js
+++ routes/customer.js
@@ -4,7 +4,8 @@ var _ = require('lodash-node');
 
 /* GET home page. */
 router.get('/gridData', function(req, res) {
-    var result= [
+    // req.params
+    var data = [
         {id:1,name: "Moroni", age: 50},
         {id:2,name: "Tiancum", age: 43},
         {id:3,name: "Jacob", age: 27},
@@ -18,31 +19,35 @@ router.get('/gridData', function(req, res) {
         {id:11,name: "Nephi", age: 29},
         {id:12,name: "Nephi", age: 29},
         {id:13,name: "Nephi", age: 29},
-        {id:14,name: "Nephi", age: 29},
-        {id:15,name: "Nephi", age: 29},
-        {id:16,name: "Nephi", age: 29},
-        {id:17,name: "Nephi", age: 29},
-        {id:18,name: "Nephi", age: 29},
-        {id:19,name: "Nephi", age: 29},
-        {id:20,name: "Nephi", age: 29},
-        {id:21,name: "Nephi", age: 29},
-        {id:22,name: "Nephi", age: 29},
-        {id:23,name: "Nephi", age: 29},
-        {id:24,name: "Nephi", age: 29},
-        {id:25,name: "Nephi", age: 29},
-        {id:26,name: "Nephi", age: 29},
-        {id:27,name: "Nephi", age: 29},
+        {id: 14, name: "Nephi", age: 15},
+        {id: 15, name: "Nephi", age: 15},
+        {id: 16, name: "Nephi", age: 15},
+        {id: 17, name: "Nephi", age: 15},
+        {id: 18, name: "Nephi", age: 15},
+        {id: 19, name: "Nephi", age: 15},
+        {id: 20, name: "Nephi", age: 15},
+        {id: 21, name: "Nephi", age: 15},
+        {id: 22, name: "Nephi", age: 15},
+        {id: 23, name: "Nephi", age: 15},
+        {id: 24, name: "Nephi", age: 15},
+        {id: 25, name: "Nephi", age: 15},
+        {id: 26, name: "Nephi", age: 15},
+        {id: 27, name: "Nephi", age: 15},
         {id:28,name: "Nephi", age: 29},
         {id:29,name: "Nephi", age: 29},
         {id:30,name: "Enos", age: 34}] ;
 
+    var result = _.filter(data, function (temp) {
+        return temp.age == 20;
+    });
     res.send(result);
 
 });
 
 router.get('/gridColumns', function(req, res) {
-    var result= [ {field: 'id', displayName:'Sr'},
-                {field: 'name', displayName:'Name'},
+    var result = [
+        {field: 'id', displayName: 'Sr', headerCellTemplate: 'Sr <br><input type="text" ng-model="ak"/>'},
+        {field: 'name', displayName:'Name'},
                 {field: 'age', displayName:'Age'}] ;
 
     res.send(result);
